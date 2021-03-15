de02ea185290e5b8be69ea3b9c4e1f21f85c3fe3
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-03-15 14:39:56 -0300

    Refatoração: criando component Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,51 @@
-export function Content() {
+import { useEffect, useState } from 'react';
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(props.selectedGenre);
+  const [movies, setMovies] = useState<MovieProps[]>(props.movies);
+  
+  useEffect(() => {
+    setSelectedGenre(props.selectedGenre);
+    setMovies(props.movies);
+  }, [props]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }

9d775547f92404e153173cfb98e1db881bd0a773
Ítalo Henrique <italohenrique014@gmail.com>
2021-12-05 20:43:48 -0300

    Adding memo to Button Component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -30,6 +30,7 @@ export function SideBar(props: SideBarProps) {
       <div className="buttons-container">
         {genres.map(genre => (
           <Button
+            key={genre.id}
             id={String(genre.id)}
             title={genre.title}
             iconName={genre.name}
